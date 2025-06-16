ce0d7b09ab15ec297c255432c44ff592562e87c4
Emmanuel Calistus <Emmanuelcailstus5@gmail.com>
2025-06-16 11:38:53 +0100

    Completed FCC Stock Checker project with MongoDB integration

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const handleStockRequest = require('./stockdata'); // assuming same folder
 
-router.get('/', getStockData);
+router.get('/api/stock-prices', getStockData);
 
 module.exports = router;
 
